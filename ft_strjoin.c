450e628175a8568db9879551fe011dc9555fcc10
xnxxsparky <benjamin.causseque@gmail.com>
2024-10-04 02:55:37 +0200

    minor adaptation join

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: bcausseq <bcausseq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 19:19:34 by bcausseq          #+#    #+#             */
-/*   Updated: 2024/10/04 02:32:18 by bcausseq         ###   ########.fr       */
+/*   Updated: 2024/10/04 02:53:30 by bcausseq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,11 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	size_t	size;
 	char	*ret;
 
 	if (!s1 || !s2)
 		return (0);
-	size = ft_strlen(s1) + ft_strlen(s2);
-	ret = (char *)malloc((size + 1) * sizeof(char));
-	*ret = 0;
+	ret = ft_strnew(ft_strlen(s1) + ft_strlen(s2));
 	if (!ret)
 		return (0);
 	strcat(ret, s1);
